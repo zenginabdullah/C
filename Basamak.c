4eaf19db514a4767ed5e33358b3b4a5ae1c95de2
zenginabdullah <117147369+zenginabdullah@users.noreply.github.com>
2022-11-17 21:10:49 +0300

    Update Basamak.c

--- Basamak.c
+++ Basamak.c
@@ -1,33 +1,17 @@
 #include <stdio.h>
 int main()
 {
- int sayi,toplam,bas1,bas2,bas3,bas4;
-  printf("4 basamakli ve rakamlari farkli bir sayi gir : ");
-  scanf("%d",&sayi);
+ int sayi,basamak;
+ printf(">");
+ scanf("%d",&sayi);
 
- if(999<sayi&&sayi<10000)
-{
-  bas1=sayi%10;
-  bas2=sayi%100-bas1;
-  bas3=sayi%1000-bas1-bas2;
-  bas4=sayi%10000-bas1-bas2-bas3;
-  bas2=bas2/10;
-  bas3=bas3/100;
-  bas4=bas4/1000;
-  if(bas1==bas2||bas1==bas3||bas1==bas4||bas2==bas3||bas2==bas4||bas3==bas4)
-  {
-  printf("Girdiginiz sayinin rakamlari farkli olmali !");
-  }
-  else
-  {toplam=bas1+bas2+bas3+bas4;
-
-  printf("Birler basamagi : %d\nOnlar basamagi : %d\nYuzler basamagi : %d\nBinler basamagi : %d",bas1,bas2,bas3,bas4);
-  printf("\nBasamaklar toplami : %d",toplam);
-  }
+ for(int i=0;i<=sayi;i++)
+ {
+  basamak=sayi%10;
+  sayi=sayi-basamak;
+  sayi=sayi/10;
+  printf("Basamak : %d\n",basamak);
  }
 
-  else 
-   printf("Girdiginiz sayi 4 basamakli olmali !");
-  
 return 0;
 }
